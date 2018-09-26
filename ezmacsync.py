56817f13a8f6a21eef29ed64dbad1030493db408
Ting Zhou <zhouting@umich.edu>
2018-09-26 15:46:55 -0700

    fix mas apps need removal

--- ezmacsync.py
+++ ezmacsync.py
@@ -4,7 +4,6 @@ __license__ = "MIT"
 __version__ = "1.0.0"
 __email__ = "ztlevi1993@gmail.com"
 
-
 import json
 import os
 import subprocess
@@ -14,7 +13,7 @@ def create_file_if_not_exist(file, initial_data=None):
     if not os.path.exists(file):
         with open(file, 'w+') as f:
             if initial_data:
-                json.dump(initial_data, f)
+                json.dump(initial_data, f, indent=2)
 
 
 if __name__ == "__main__":
@@ -22,8 +21,8 @@ if __name__ == "__main__":
 
     # get ignore_lists configs
     ignore_lists_file = home + '/.ezmacsyncrc'
-    initial_ignore_lists = {"cloudDir": "", "brewIgnoreList": [
-    ], "brewCaskIgnoreList": [], "masIgnoreList": []}
+    initial_ignore_lists = {"cloudDir": "", "brewRemoveList": [
+    ], "brewCaskRemoveList": [], "masRemoveList": []}
     create_file_if_not_exist(ignore_lists_file, initial_ignore_lists)
     with open(ignore_lists_file) as f:
         ignore_lists = json.load(f)
@@ -34,7 +33,7 @@ if __name__ == "__main__":
         ignore_lists['cloudDir'] = cloud_path
     cloud_path = ignore_lists['cloudDir']
     with open(ignore_lists_file, 'w+') as f:
-        json.dump(ignore_lists, f)
+        json.dump(ignore_lists, f, indent=2)
 
     cloud_dir = home + '/' + cloud_path
     if not os.path.isdir(cloud_dir):
@@ -63,7 +62,7 @@ if __name__ == "__main__":
         '/usr/local/bin/brew list', shell=True).strip('\n').split('\n')
     brew_apps = list(set(brew_apps) | set(synced_lists['brewAppList']))
     brew_ignore_set = set(
-        map(lambda app: app.lower(), ignore_lists['brewIgnoreList']))
+        map(lambda app: app.lower(), ignore_lists['brewRemoveList']))
     brew_apps = filter(lambda app: app.lower()
                        not in brew_ignore_set, brew_apps)
     synced_lists['brewAppList'] = brew_apps
@@ -75,7 +74,7 @@ if __name__ == "__main__":
     brew_cask_apps = list(set(brew_cask_apps) | set(
         synced_lists['brewCaskAppList']))
     brew_cask_ignore_set = set(
-        map(lambda app: app.lower(), ignore_lists['brewCaskIgnoreList']))
+        map(lambda app: app.lower(), ignore_lists['brewCaskRemoveList']))
     brew_cask_apps = filter(lambda app: app.lower(
     ) not in brew_cask_ignore_set, brew_cask_apps)
     synced_lists['brewCaskAppList'] = brew_cask_apps
@@ -88,18 +87,20 @@ if __name__ == "__main__":
     def split_mas_app(app):
         i1 = app.find(' ')
         i2 = app.find('(')
-        return (app[:i1], app[i1+1:i2-1])
+        return (app[:i1], app[i1 + 1:i2 - 1])
+
     mas_apps = map(split_mas_app, mas_apps)
+    mas_apps_local = list(mas_apps)
     mas_apps = list(set(mas_apps) | set(
         map(lambda i: tuple(i), synced_lists['masAppList'])))
     mas_ignore_set = set(
-        map(lambda app: app.lower(), ignore_lists['masIgnoreList']))
+        map(lambda app: app.lower(), ignore_lists['masRemoveList']))
     mas_apps = filter(lambda app: app[1].lower()
                       not in mas_ignore_set, mas_apps)
     synced_lists['masAppList'] = mas_apps
 
     with open(synced_lists_file, 'w+') as f:
-        json.dump(synced_lists, f)
+        json.dump(synced_lists, f, indent=2)
 
     print('================ Backup End ================')
 
@@ -107,17 +108,19 @@ if __name__ == "__main__":
     install_script = ''
     # brew apps install script
     install_script += 'brew install ' + \
-        ' '.join(synced_lists['brewAppList']) + '\n'
-    if ignore_lists['brewIgnoreList']:
+                      ' '.join(synced_lists['brewAppList']) + '\n'
+    if ignore_lists['brewRemoveList']:
         install_script += 'brew uninstall ' + \
-            ' '.join(ignore_lists['brewIgnoreList']) + ' --force\n'
+                          ' '.join(
+                              ignore_lists['brewRemoveList']) + ' --force\n'
 
     # brew cask apps install script
     install_script += 'brew cask install ' + \
-        ' '.join(synced_lists['brewCaskAppList']) + '\n'
-    if ignore_lists['brewCaskIgnoreList']:
+                      ' '.join(synced_lists['brewCaskAppList']) + '\n'
+    if ignore_lists['brewCaskRemoveList']:
         install_script += 'brew cask uninstall ' + \
-            ' '.join(ignore_lists['brewCaskIgnoreList']) + ' --force\n'
+                          ' '.join(
+                              ignore_lists['brewCaskRemoveList']) + ' --force\n'
 
     # mas apps install script
     for app in synced_lists['masAppList']:
@@ -127,7 +130,8 @@ if __name__ == "__main__":
     os.system(install_script)
 
     mas_apps_need_removal = filter(
-        lambda app: app[1].lower() in mas_ignore_set, mas_apps)
+        lambda app: app[1].lower() in mas_ignore_set, mas_apps_local)
+    mas_apps_need_removal = map(lambda app: app[1], mas_apps_need_removal)
     if mas_apps_need_removal:
         print('\nYou might need to remove {} manually.'.format(
             ' '.join(mas_apps_need_removal)))
