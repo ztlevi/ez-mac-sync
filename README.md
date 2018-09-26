07f4ed128a31516c810a4e781449ea832d003a87
Ting Zhou <zhouting@umich.edu>
2018-09-26 13:05:41 -0700

    init

---
+++ README.md
@@ -0,0 +1,56 @@
+# EZ-MAC-SYNC
+
+## Prerequisites
+
+1. [Homebrew](https://brew.sh/)
+2. [mas](https://github.com/mas-cli/mas)
+3. A cloud service, e.g. Dropbox, Google Drive, iCloud
+
+Scripts to install prerequisites
+
+```shell
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Mas
+brew install mas
+```
+
+## Usage
+
+1. Download the source code, then `cd` into `ez-mac-sync`.
+2. `$ chmod +x ./ezmacsync.py`
+3. `./ezmacsync.py`
+4. You can alias this script in your shell setting, e.g. `alias update-mac=~/ez-mac-synd/ezmacsync.py`.
+
+For the fist time, you need to input your cloud directory (relative to your HOME path). For example, `Dropbox/AppList`, and the syncing file will be saved under `$HOME/Dropbox/AppList`.
+
+A `.ezmacsyncrc` file will be created under your HOME directory. You can put applications you want to remove across all your mac devices in this file.
+
+```json
+{
+  "cloudDir": "Dropbox/AppList",
+  "brewIgnoreList": [],
+  "masIgnoreList": ["Final Cut Pro", "Compressor", "Motion", "Logic Pro X", "MainStage 3"],
+  "brewCaskIgnoreList": ["eclipse-jee", "android-studio", "gitkraken"]
+}
+```
+
+> **Note**: `mas` applications will not be removed from your mac because `mas` cannot uninstall apps, but it will notify you the apps you might need to manually uninstall.
+
+## Upgrading Apps
+
+For upgrading brew apps, use `bubu`
+
+```shell
+alias bubo=brew update && brew outdated
+alias bubc=brew upgrade && brew cleanup
+alias bubu=bubo && bubc
+```
+
+For upgrading brew cask apps, see [homebrew-cask-upgrade](https://github.com/buo/homebrew-cask-upgrade).
+
+```shell
+brew tap buo/cask-upgrade
+brew cu
+```
---
+++ ezmacsync.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+__author__ = "Ting Zhou"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__email__ = "ztlevi1993@gmail.com"
+
+
+import json
+import os
+import subprocess
+
+
+def create_file_if_not_exist(file, initial_data=None):
+    if not os.path.exists(file):
+        with open(file, 'w+') as f:
+            if initial_data:
+                json.dump(initial_data, f)
+
+
+if __name__ == "__main__":
+    home = os.path.expanduser("~")
+
+    # get ignore_lists configs
+    ignore_lists_file = home + '/.ezmacsyncrc'
+    initial_ignore_lists = {"cloudDir": "", "brewIgnoreList": [
+    ], "brewCaskIgnoreList": [], "masIgnoreList": []}
+    create_file_if_not_exist(ignore_lists_file, initial_ignore_lists)
+    with open(ignore_lists_file) as f:
+        ignore_lists = json.load(f)
+        print("ignore_lists.json file loaded...")
+    if 'cloudDir' not in ignore_lists or not ignore_lists['cloudDir']:
+        cloud_path = raw_input(
+            'Input your cloud directory relative to $HOME for syncing...\ne.g. Dropbox/AppList\n')
+        ignore_lists['cloudDir'] = cloud_path
+    cloud_path = ignore_lists['cloudDir']
+    with open(ignore_lists_file, 'w+') as f:
+        json.dump(ignore_lists, f)
+
+    cloud_dir = home + '/' + cloud_path
+    if not os.path.isdir(cloud_dir):
+        os.makedirs(cloud_dir)
+
+    # get synced_lists data
+    synced_lists_file = cloud_dir + "/synced_lists.json"
+    initial_synced_lists = {"allAppList": [], "brewAppList": [
+    ], "brewCaskAppList": [], "masAppList": []}
+    create_file_if_not_exist(synced_lists_file, initial_synced_lists)
+    with open(synced_lists_file) as f:
+        synced_lists = json.load(f)
+        print('synced_lists.json file loaded...')
+
+    print('================ Backup Start ================')
+
+    # backup /Applications
+    print('Backup /Applications...')
+    all_apps = list(set(os.listdir('/Applications'))
+                    | set(synced_lists["allAppList"]))
+    synced_lists["allAppList"] = all_apps
+
+    # backup brew apps
+    print('Backup brew apps...')
+    brew_apps = subprocess.check_output(
+        '/usr/local/bin/brew list', shell=True).strip('\n').split('\n')
+    brew_apps = list(set(brew_apps) | set(synced_lists['brewAppList']))
+    brew_ignore_set = set(
+        map(lambda app: app.lower(), ignore_lists['brewIgnoreList']))
+    brew_apps = filter(lambda app: app.lower()
+                       not in brew_ignore_set, brew_apps)
+    synced_lists['brewAppList'] = brew_apps
+
+    # backup brew cask apps
+    print('Backup brew cask apps...')
+    brew_cask_apps = subprocess.check_output(
+        '/usr/local/bin/brew cask list', shell=True).strip('\n').split('\n')
+    brew_cask_apps = list(set(brew_cask_apps) | set(
+        synced_lists['brewCaskAppList']))
+    brew_cask_ignore_set = set(
+        map(lambda app: app.lower(), ignore_lists['brewCaskIgnoreList']))
+    brew_cask_apps = filter(lambda app: app.lower(
+    ) not in brew_cask_ignore_set, brew_cask_apps)
+    synced_lists['brewCaskAppList'] = brew_cask_apps
+
+    # backup mas apps
+    print('Backup mas apps...')
+    mas_apps = subprocess.check_output(
+        '/usr/local/bin/mas list', shell=True).strip('\n').split('\n')
+
+    def split_mas_app(app):
+        i1 = app.find(' ')
+        i2 = app.find('(')
+        return (app[:i1], app[i1+1:i2-1])
+    mas_apps = map(split_mas_app, mas_apps)
+    mas_apps = list(set(mas_apps) | set(
+        map(lambda i: tuple(i), synced_lists['masAppList'])))
+    mas_ignore_set = set(
+        map(lambda app: app.lower(), ignore_lists['masIgnoreList']))
+    mas_apps = filter(lambda app: app[1].lower()
+                      not in mas_ignore_set, mas_apps)
+    synced_lists['masAppList'] = mas_apps
+
+    with open(synced_lists_file, 'w+') as f:
+        json.dump(synced_lists, f)
+
+    print('================ Backup End ================')
+
+    print('================ Install Start =================')
+    install_script = ''
+    # brew apps install script
+    install_script += 'brew install ' + \
+        ' '.join(synced_lists['brewAppList']) + '\n'
+    if ignore_lists['brewIgnoreList']:
+        install_script += 'brew uninstall ' + \
+            ' '.join(ignore_lists['brewIgnoreList']) + ' --force\n'
+
+    # brew cask apps install script
+    install_script += 'brew cask install ' + \
+        ' '.join(synced_lists['brewCaskAppList']) + '\n'
+    if ignore_lists['brewCaskIgnoreList']:
+        install_script += 'brew cask uninstall ' + \
+            ' '.join(ignore_lists['brewCaskIgnoreList']) + ' --force\n'
+
+    # mas apps install script
+    for app in synced_lists['masAppList']:
+        install_script += 'mas install ' + app[0] + '\n'
+
+    # exec the install script
+    os.system(install_script)
+
+    mas_apps_need_removal = filter(
+        lambda app: app[1].lower() in mas_ignore_set, mas_apps)
+    if mas_apps_need_removal:
+        print('\nYou might need to remove {} manually.'.format(
+            ' '.join(mas_apps_need_removal)))
+
+    print('\n================ Install End =================')
